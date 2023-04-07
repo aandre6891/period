472c3b5077a11f6b84f0d52b49d0c78584dc4bcb
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-07 18:00:00 +0100

    add methods

--- period.rb
+++ period.rb
@@ -1,17 +1,31 @@
-#  get the date
-#  ask how many days is the usual period
-#  ask when the last period ended
-#  calculate the date of the next period
-#  calculate how many days still remain
 require 'date' 
-puts "Welcome to the period calculator! What's your name?".center(100, "-")
-name = gets.chomp.capitalize
-puts "Hello #{name}! Today is the #{Date.today}.".center(100, "-")
-require 'date' 
-puts "Please #{name}, enter the date when your last period started in this format: (MM/DD/YYYY)"
-lastP = gets.chomp 
-start_date = Date.strptime(lastP, "%d/%m") 
-puts "How many days is your period cycle usually?"
-duration = gets.chomp.to_i 
-next_period = start_date.next_day(duration)  
-puts "Your next period should start on the #{next_period.strftime("%d/%m/%Y")}."
+
+def welcome
+  puts "Welcome to the period calculator! What's your name?".center(100, "-")
+  @name = gets.chomp.capitalize
+  puts "Hello #{@name}! Today is the #{DateTime.now.strftime("%d/%m/%Y")}.".center(100, "-")
+  get_last_period
+end
+
+def get_last_period
+  puts "Please #{@name}, enter the date when your last period started in this format: (MM/DD/YYYY)"
+  @last_period = Date.strptime(gets.chomp, "%d/%m") 
+  cycle_length
+end
+
+def cycle_length
+  puts "How many days is your period cycle usually?"
+  @duration = gets.chomp.to_i 
+  next_period_calc
+end
+
+def next_period_calc
+  @next_period = @last_period.next_day(@duration)  
+  print_result
+end
+
+def print_result
+    puts "Your next period should start on the #{@next_period.strftime("%d/%m/%Y")}."
+end
+
+welcome
