ee8904f9e5a3c9706059661757dce651927d376d
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-07 17:39:12 +0100

    first draft

--- period.rb
+++ period.rb
@@ -3,3 +3,15 @@
 #  ask when the last period ended
 #  calculate the date of the next period
 #  calculate how many days still remain
+require 'date' 
+puts "Welcome to the period calculator! What's your name?".center(100, "-")
+name = gets.chomp.capitalize
+puts "Hello #{name}! Today is the #{Date.today}.".center(100, "-")
+require 'date' 
+puts "Please #{name}, enter the date when your last period started in this format: (MM/DD/YYYY)"
+lastP = gets.chomp 
+start_date = Date.strptime(lastP, "%d/%m") 
+puts "How many days is your period cycle usually?"
+duration = gets.chomp.to_i 
+next_period = start_date.next_day(duration)  
+puts "Your next period should start on the #{next_period.strftime("%d/%m/%Y")}."
